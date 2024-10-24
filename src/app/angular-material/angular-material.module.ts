dfd8610e5d580f48845c1e60c5f21dc8409a75ca
Your Name <you@example.com>
2024-10-25 00:40:53 +0200

    First Commit

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {CdkDrag, CdkDropList , CdkDragHandle} from '@angular/cdk/drag-drop';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatTableModule} from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CdkDrag,
+    CdkDropList,
+    DragDropModule,
+    CdkDragHandle,
+    MatSlideToggleModule,
+    MatTableModule
+    
+
+  
+
+
+  ],
+  exports: [
+    CommonModule,
+    CdkDrag,
+    CdkDropList,
+    DragDropModule,
+    CdkDragHandle,
+    MatSlideToggleModule,
+    MatTableModule
+  ]
+})
+export class AngularMaterialModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component, EventEmitter } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppModule } from './app.module';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { CdkDragDrop, DragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,
+    AppModule,
+    AngularMaterialModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Dev-intership';
+  // colornumber: number  = 0;
+
+  squareColor: string = "lightblue"
+  clickCount: number = 0
+  undocolor: string[] = []
+  undoColorDragDrop: string[] = []
+  maxUndoColor: number = 1
+  redocolor: string[] = []
+  redoColorDragDrop: string[] = []
+  colors: string[] = []
+
+  ClickChangeColor(): void {
+    this.clickCount++;
+    this.squareColor = this.RandomColor()
+    console.log(this.squareColor)
+  }
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.colors, event.previousIndex, event.currentIndex);
+
+  }
+
+
+  RandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    if (this.undocolor.length >= this.maxUndoColor) {
+      this.undocolor.shift();
+      this.redocolor.shift()
+
+    }
+
+    if (this.squareColor) {
+      this.undocolor.push(this.squareColor);
+      this.redocolor.push(color)
+      this.colors.push(color)
+    }
+
+    for(let i = 0 ; i <= this.undocolor.length ; i++){
+      let undo = this.undocolor[i]
+      this.undoColorDragDrop.push(undo);
+    }
+
+    for(let i = 0 ; i <= this.redocolor.length ; i++){
+      let redo = this.redocolor[i]
+      this.redoColorDragDrop.push(redo);
+    }
+   
+
+
+    console.log(this.undocolor)
+    return color
+  }
+
+
+  onUndoClick() {
+    this.squareColor = this.undocolor[0]
+  }
+
+  onRedoClick() {
+    this.squareColor = this.redocolor[0]
+  }
+}
